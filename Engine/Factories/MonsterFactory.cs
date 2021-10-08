9e46851d58caba9f5f14bbedf6c41e15a920fd6d
programmerJ <programmerJ@outlook.com>
2021-10-08 00:04:42 -0400

    add min and max damage to Monster.cs  and MonsterFactory.cs

--- Engine/Factories/MonsterFactory.cs
+++ Engine/Factories/MonsterFactory.cs
@@ -11,7 +11,7 @@ namespace Engine.Factories
             {
                 case 1:
                     Monster snake =
-                        new Monster("Snake", "Snake.png", 4, 4, 5, 1);
+                        new Monster("Snake", "Snake.png", 4, 4, 1, 2, 5, 1);// change to pass values for these parameters(min&max Damage) when we instantiate a Monster object
 
                     AddLootItem(snake, 9001, 25);
                     AddLootItem(snake, 9002, 75);
@@ -20,7 +20,7 @@ namespace Engine.Factories
 
                 case 2:
                     Monster rat =
-                        new Monster("Rat", "Rat.png", 5, 5, 5, 1);
+                        new Monster("Rat", "Rat.png", 5, 5, 1, 2, 5, 1);// change to pass values for these parameters(min&max Damage) when we instantiate a Monster object
 
                     AddLootItem(rat, 9003, 25);
                     AddLootItem(rat, 9004, 75);
@@ -29,7 +29,7 @@ namespace Engine.Factories
 
                 case 3:
                     Monster giantSpider =
-                        new Monster("Giant Spider", "GiantSPider.png", 10, 10, 10, 3);
+                        new Monster("Giant Spider", "GiantSPider.png", 10, 10, 1, 4, 10, 3);// change to pass values for these parameters(min&max Damage) when we instantiate a Monster object
 
                     AddLootItem(giantSpider, 9005, 25);
                     AddLootItem(giantSpider, 9006, 75);
@@ -38,7 +38,6 @@ namespace Engine.Factories
 
                 default:
                     throw new ArgumentException(string.Format("MonsterType '{0}' does not exist", monsterID));
-
             }
         }
 
--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -7,7 +7,7 @@ namespace Engine.Models
         private int hitPoints;
 
         public string Name { get; private set; }
-        public string ImageName { get; }
+        public string ImageName { get; set; }
         public int MaximumHitPoints { get; private set; }
         public int HitPoints
         {
@@ -18,6 +18,8 @@ namespace Engine.Models
                 OnPropertyChanged(nameof(HitPoints));
             }
         }
+        public int MinimumDamage { get; set; }// add two new properties to the Monster class: MinimumDamage and MaximumDamage (both integer properties)
+        public int MaximumDamage { get; set; }// add two new properties to the Monster class: MinimumDamage and MaximumDamage (both integer properties)
 
         public int RewardExperiencePoints { get; private set; }
         public int RewardGold { get; private set; }
@@ -26,12 +28,15 @@ namespace Engine.Models
 
         public Monster(string name, string imageName,
             int maximumHitPoints, int hitPoints,
+            int minimumDamage, int maximumDamage,// To populate these properties, we’ll add two new parameters to the Monster constructor
             int rewardExperiencePoints, int rewardGold)
         {
             Name = name;
             ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
             MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
+            MinimumDamage = minimumDamage;// and set the properties with the passed-in parameter values
+            MaximumDamage = maximumDamage;// and set the properties with the passed-in parameter values
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
 
