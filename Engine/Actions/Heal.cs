1570b7f83ee2cf0826177a02be48053e801ec9a1
programmerJ <programmerJ@outlook.com>
2021-11-28 15:31:53 -0500

    add BaseAction.cs in Engine.Actions folder add :base(itemInUse) to Heal, add  : BaseAction to public class Heal, & change GameItem item to GameItem itemInUse in Heal.cs add : BaseAction to class AttackWithWeapon, ::base(itemInUse) to AttackWithWeapon, & remove ReportResult Method from AttackWithWeapon.cs

--- Engine/Actions/Heal.cs
+++ Engine/Actions/Heal.cs
@@ -3,21 +3,18 @@ using Engine.Models;
 
 namespace Engine.Actions
 {
-    public class Heal : IAction
+    public class Heal : BaseAction, IAction
     {
-        private readonly GameItem _item;
         private readonly int _hitPointsToHeal;
-
-        public event EventHandler<string> OnActionPerformed;
         
-        public Heal(GameItem item, int hitPointsToHeal)// Heal constructor. Takes in item & hitPointsToHeal
+        public Heal(GameItem itemInUse, int hitPointsToHeal) // Heal constructor. Takes in item & hitPointsToHeal
+            : base(itemInUse)
         {
-            if (item.Category != GameItem.ItemCategory.Consumable)
+            if (itemInUse.Category != GameItem.ItemCategory.Consumable)
             {
-                throw new ArgumentException($"{item.Name} is not consumable!");
+                throw new ArgumentException($"{itemInUse.Name} is not consumable!");
             }
 
-            _item = item;
             _hitPointsToHeal = hitPointsToHeal;
         }
 
@@ -29,12 +26,5 @@ namespace Engine.Actions
             ReportResult($"{actorName} heal {targetName} for {_hitPointsToHeal} point{(_hitPointsToHeal > 1 ? "s" : "")}.");
             target.Heal(_hitPointsToHeal);// I would like to change the message to say "You use Granola bar and heal X points of damage
         }
-
-        private void ReportResult(string result)
-        {
-            OnActionPerformed?.Invoke(this, result);
-        }
-
-
     }
 }
